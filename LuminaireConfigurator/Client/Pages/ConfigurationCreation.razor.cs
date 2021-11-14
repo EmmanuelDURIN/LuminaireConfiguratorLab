c1c19844518960a89f46d1b89a88425002f48551
Emmanuel <emmanueldurin@gmail.com>
2021-11-14 22:24:41 +0100

    ConfigurationCreation

---
+++ LuminaireConfigurator/Client/Pages/ConfigurationCreation.razor.cs
@@ -0,0 +1,12 @@
+﻿using LuminaireConfigurator.ViewModel;
+
+namespace LuminaireConfigurator.Client.Pages
+{
+  public partial class ConfigurationCreation
+  {
+    public LuminaireConfiguration Configuration { get; set; } = new LuminaireConfiguration();
+    public void Create()
+    {
+    }
+  }
+}
---
+++ LuminaireConfigurator/Client/ViewModel/LuminaireConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LuminaireConfigurator.ViewModel
+{
+  public class LuminaireConfiguration
+  {
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Optic { get; set; } = "NM10";
+    [Range(1, 1E10)]
+    public double LampFlux { get; set; } = 5000;
+    [Range(1, 1E6)]
+    public int LampColor { get; set; } = 5400;
+  }
+}
